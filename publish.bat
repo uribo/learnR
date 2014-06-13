2050f603aa86fca24ee3ada8d6272acf9e6d8ee0
Kun Ren <renkun@outlook.com>
2014-06-13 09:03:14 +0800

    publish updated

--- publish.bat
+++ publish.bat
@@ -6,7 +6,7 @@ git commit --allow-empty -m "Update built gitbook"
 git checkout -b gh-pages
 git add .
 git commit -am "Update built gitbook"
-git push https://github.com/renkun-ken/learnR.git gh-pages --force
+git push git@github.com:renkun-ken/learnR.git gh-pages --force
 git checkout master
 git branch -D gh-pages
 cd ..
