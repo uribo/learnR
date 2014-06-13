592e89a907a83a406a4133a621d81d4800e8648d
Kun Ren <renkun@outlook.com>
2014-06-13 12:06:01 +0800

    publish updated

--- publish.bat
+++ publish.bat
@@ -1,12 +1,11 @@
-rmdir _book /S /Q
-rscript publish.R
-cd _book
-git init
-git commit --allow-empty -m "Update built gitbook"
-git checkout -b gh-pages
-git add .
-git commit -am "Update built gitbook"
-git push git@github.com:renkun-ken/learnR.git gh-pages --force
-git checkout master
-git branch -D gh-pages
-cd ..
+@cd /d %~dp0
+@rscript publish.R
+@cd _book
+@git init
+@git add .
+@git commit --allow-empty -m "Update built gitbook"
+@git checkout -b gh-pages
+@git push git@github.com:renkun-ken/learnR.git gh-pages --force
+@git checkout master
+@git branch -D gh-pages
+@cd ..
