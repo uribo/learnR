493e4c2695ff4cb114e731c19cffb288f082ca90
Kun Ren <renkun@outlook.com>
2014-06-14 23:25:14 +0800

    publish via https and ssh

---
+++ publish.https.bat
@@ -0,0 +1,11 @@
+@cd /d %~dp0
+@rscript publish.R
+@cd _book
+@git init
+@git add .
+@git commit --allow-empty -m "Update built gitbook"
+@git checkout -b gh-pages
+@git push https://github.com/renkun-ken/learnR.git gh-pages --force
+@git checkout master
+@git branch -D gh-pages
+@cd ..
